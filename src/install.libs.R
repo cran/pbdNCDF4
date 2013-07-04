eef65ed8353920c5fda8ec0274d9c8cd5e6a6c7b
Pragneshkumar Patel <pragnesh@utk.edu>
2013-07-04 00:00:00 +0000

    version 0.1-1

--- src/install.libs.R
+++ src/install.libs.R
@@ -1,14 +1,13 @@
 ### Modified from Rserve/src/install.libs.R
 ### For libs
-files <- c("pbdNCDF4.so", "pbdNCDF4.so.dSYM", "pbdNCDF4.dylib", "pbdNCDF4.dll")
+files <- c("pbdNCDF4.so", "pbdNCDF4.so.dSYM", "pbdNCDF4.dylib", "pbdNCDF4.dll",
+           "symbols.rds")
 files <- files[file.exists(files)]
 if(length(files) > 0){
   libsarch <- if (nzchar(R_ARCH)) paste("libs", R_ARCH, sep='') else "libs"
   dest <- file.path(R_PACKAGE_DIR, libsarch)
   dir.create(dest, recursive = TRUE, showWarnings = FALSE)
   file.copy(files, dest, overwrite = TRUE, recursive = TRUE)
-  if(file.exists("symbols.rds"))
-    file.copy("symbols.rds", dest, overwrite = TRUE)
 }
 ### For etc
 file <- "Makeconf"
